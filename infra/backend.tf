767a81220161f804640ea3e01d1ab0ead1b8ccad
Michael Harris Jr. <michael61906@gmail.com>
2026-09-20 13:13:16 -0500

    feat(infra): add remote backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "s3" {
-        bucket = "mharris-tf-state-597341305438-us-east-1-an"
-        key = "terraform.tfstate"
-        region = "us-east-1"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket  = "mharris-tf-state-597341305438-us-east-1-an"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
