a5322ab37a5435490a2bd9858049d5174f9ae5b7
Michael Harris Jr. <michael61906@gmail.com>
2026-09-20 13:48:28 -0500

    refactor(infra): add image lifecycle policy

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -15,3 +15,26 @@ resource "aws_ecr_repository" "weather_dbt" {
     scan_on_push = true
   }
 }
+
+data "aws_ecr_lifecycle_policy_document" "main" {
+  rule {
+    priority    = 1
+    description = "Keep at most five images."
+
+    selection {
+      tag_status   = "any"
+      count_type   = "imageCountMoreThan"
+      count_number = 5
+    }
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "weather_scrape" {
+  repository = aws_ecr_repository.weather_scrape.name
+  policy     = data.aws_ecr_lifecycle_policy_document.main.json
+}
+
+resource "aws_ecr_lifecycle_policy" "weather_dbt" {
+  repository = aws_ecr_repository.weather_dbt.name
+  policy     = data.aws_ecr_lifecycle_policy_document.main.json
+}
