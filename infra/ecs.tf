3abfecf71565bd6211c3c3e7b2918b9760984b94
Michael Harris Jr. <michael61906@gmail.com>
2026-09-21 08:32:27 -0500

    feat(infra): add ecs task definitions

---
+++ infra/ecs.tf
@@ -0,0 +1,110 @@
+resource "aws_ecs_task_definition" "hourly_forecasted_weather" {
+  family                   = "${var.project_name}-scrape-hourly-forecasted-weather"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      name      = "scrape"
+      image     = "${aws_ecr_repository.scrape.repository_url}:${var.image_tag}"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      environment : [
+        { "name" : "WHAS_WEATHER_RAW_S3_BUCKET", "value" : aws_s3_bucket.weather_raw.bucket }
+      ]
+      command : ["--type", "forecast", "--frequency", "hourly"]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn = aws_iam_role.ecs_task_scrape
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
+
+resource "aws_ecs_task_definition" "daily_forecasted_weather" {
+  family                   = "${var.project_name}-scrape-daily-forecasted-weather"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      name      = "scrape"
+      image     = aws_ecr_repository.scrape.repository_url
+      cpu       = 256
+      memory    = 512
+      essential = true
+      environment : [
+        { "name" : "WHAS_WEATHER_RAW_S3_BUCKET", "value" : aws_s3_bucket.weather_raw.bucket }
+      ]
+      command : ["--type", "forecast", "--frequency", "daily"]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn = aws_iam_role.ecs_task_scrape
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
+
+resource "aws_ecs_task_definition" "hourly_observed_weather" {
+  family                   = "${var.project_name}-scrape-hourly-observed-weather"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      name      = "scrape"
+      image     = "${aws_ecr_repository.scrape.repository_url}:${var.image_tag}"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      environment : [
+        { "name" : "WHAS_WEATHER_RAW_S3_BUCKET", "value" : aws_s3_bucket.weather_raw.bucket }
+      ]
+      command : ["--type", "observation", "--frequency", "hourly"]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn = aws_iam_role.ecs_task_scrape
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
+
+resource "aws_ecs_task_definition" "dbt_build" {
+  family                   = "${var.project_name}-dbt-build"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      name      = "dbt"
+      image     = "${aws_ecr_repository.dbt.repository_url}:${var.image_tag}"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      environment : [
+        { "name" : "AWS_REGION", "value" : local.aws_region },
+        { "name" : "WHAS_WEATHER_ATHENA_QUERY_RESULTS_S3_BUCKET", "value" : aws_s3_bucket.athena_staging.bucket },
+        { "name" : "WHAS_WEATHER_INTERMEDIATE_S3_BUCKET", "value" : aws_s3_bucket.weather_intermediate.bucket },
+        { "name" : "WHAS_WEATHER_MART_S3_BUCKET", "value" : aws_s3_bucket.weather_mart.bucket }
+      ]
+      command : ["dbt", "build", "--select", "+marts"]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn = aws_iam_role.ecs_task_dbt_build.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
