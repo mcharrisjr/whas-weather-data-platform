620c54be12081f925c779105fdfc221480826f24
Michael Harris Jr. <michael61906@gmail.com>
2026-09-21 08:42:25 -0500

    feat(infra): add ecs task security group

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -26,3 +26,15 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.main.id
 }
+
+resource "aws_security_group" "ecs_task" {
+  name = "${var.project_name}-ecs-task-sg"
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
