db5abef3a4ae8c6fd6455215ef032d4157725ed1
Michael Harris Jr. <michael61906@gmail.com>
2026-09-23 20:22:33 -0500

    fix(dbt): mart column name

--- dbt/models/marts/mart_hourly_forecast_accuracy.sql
+++ dbt/models/marts/mart_hourly_forecast_accuracy.sql
@@ -1,7 +1,7 @@
 SELECT
-    f.forecasted_at,
+    f.forecasted_for,
     o.temperature_f - f.temperature_f AS bias_temperature_f,
     DATE_DIFF('hour', f.scraped_at, f.forecasted_at) AS forecast_horizon_hours
 FROM {{ ref('stg_hourly_forecast') }} AS f
 INNER JOIN {{ ref('int_hourly_observation') }} AS o
-    ON f.forecasted_at = o.observed_at
+    ON f.forecasted_for = o.observed_at
